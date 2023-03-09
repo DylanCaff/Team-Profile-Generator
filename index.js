80c01cafee39a3253c18e2f39157ae155196721a
DylanCaff <dylancaff0902@gmail.com>
2023-03-09 16:28:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
 const generateHTML = require('./src/generateHTML');
 const fs = require('fs');
 const inquirer = ('inquirer');
-
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
+
+const addManager = () => {
+    return inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'managerName',
+            message: "What is the team manager's name?"
+        },
+        {
+            type: 'input',
+            name: 'managerId',
+            message: "What is the team manager's id?"
+        },
+        {
+            type: 'input',
+            name: 'managerEmail',
+            message: "What is the team manager's email?"
+        },
+        {
+            type: 'input',
+            name: 'managerOfficeNumber',
+            message: "What is the team manager's office number?"
+        },
+    ])
+    .then(managerInput => {
+        const { name, id, email, officeNumber } = managerInput;
+        const manager = new Manager (name, id, email, officeNumber);
+    });
+};
 
 
 
