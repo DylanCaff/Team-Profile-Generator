0fa5d2df0dfbee6347963a1ddcc36fbff26f8d4c
DylanCaff <dylancaff0902@gmail.com>
2023-03-11 14:04:22 -0500

    made changes to index.js

--- index.js
+++ index.js
@@ -3,43 +3,78 @@ const fs = require('fs');
 
 const inquirer = ('inquirer');
 
-const Manager = require('./lib/Manager');
+const { Manager, managerQuestionsArr } = require('./lib/Manager');
 
-const Engineer = require('./lib/Engineer');
+const { Engineer, engineerQuestionsArr } = require('./lib/Engineer');
 
-const Intern = require('./lib/Intern');
+const { Intern, internQuestionsArr } = require('./lib/Intern');
 
 const teamArry = [];
 
 
+const init = () => { managerQuestions() }
+
+const managerQuestions = () => {
+    inquirer.prompt(managerQuestionsArr)
+        .then((input) => {
+            input = new Manager(input.name, input.id, input.email, input.officeNumber)
+            teamArry.push(input);
+            return addEmployee();
+        })
+};
+
+const engineerQuestions = () => {
+    inquirer.prompt(engineerQuestionsArr)
+        .then((intput) => {
+            input = new Engineer(input.name, input.id, input.email, input.github)
+            teamArry.push(input);
+            return addEmployee();
+        })
+};
+
+const internQuestions = () => {
+    inquirer.prompt(internQuestionsArr)
+        .then((intput) => {
+            input = new Intern(input.name, input.id, input.email, input.github)
+            teamArry.push(input);
+            return addEmployee();
+        })
+};
+
 
 const addEmployee = () => {
-    return inquirer.prompt([
+    inquirer.prompt([
         {
             type: 'list',
             name: 'role',
             message: "Which type of team member would you like to add?",
-            choices: ['Engineer', 'Intern', "I don't want to add anymore team members"]
+            choices: [
+                { name: 'Engineer', value: "addEngineer" },
+                { name: 'Intern', value: "addIntern" },
+                { name: 'DONE', value: "done" }
+            ]
         },
-    ])
-};
-
-
-
-
-
-
-
-const writeFile = data => {
-    fs.writeFile('./dist.index.html', data, err => {
-        if (err) {
-            console.log(err);
-            return;
-        } else {
-            console.log("Your team has been created!")
+    ]).then(input => {
+        if (input.role === 'addEngineer') { engineerQuestions(); };
+        if (input.role === 'addIntern') { internQuestions(); };
+        if (input.role === 'done') {
+            const writeFile = data => {
+                fs.writeFile('./dist.index.html', data, err => {
+                    if (err) {
+                        console.log(err);
+                        return;
+                    } else {
+                        console.log("Your team has been created!")
+                    }
+                })
+            };
         }
     })
 };
 
 
 
+
+
+
+init();
