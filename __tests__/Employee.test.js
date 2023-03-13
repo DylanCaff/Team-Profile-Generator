a1909f5913311e90edff77730563b247f96f700b
DylanCaff <dylancaff0902@gmail.com>
2023-03-13 13:56:35 -0400

    wokred on employee.test.js

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,2 +1,27 @@
 const Employee = require('../lib/Employee');
 
+
+describe('Employee class', () => {
+    test('creates new employee', () => {
+        const name = 'Dylan Cafferatta';
+        const id = 321;
+        const email = 'dylancaff0902@gmail.com';
+        const employee = new Employee(name, id, email);
+
+        expect(employee.name).toBe(name);
+        expect(employee.id).toEqual(expect.any(Number));
+        expect(employee.email).toEqual(expect.any(String));
+    });
+
+    test('checks all methods', () => {
+        const name = 'Dylan Cafferatta';
+        const id = 321;
+        const email = 'dylancaff0902@gmail.com';
+        const employee = new Employee(name, id, email);
+
+        expect(employee.getName()).toBe(name);
+        expect(employee.getId()).toBe(id);
+        expect(employee.getEmail()).toBe(email);
+        expect(employee.getRole()).toBe('Employee');
+    });
+});
