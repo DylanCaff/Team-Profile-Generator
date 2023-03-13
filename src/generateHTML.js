fc0fd56a4e73b11dd803148988cabaf396d6e621
DylanCaff <dylancaff0902@gmail.com>
2023-03-13 12:29:13 -0400

    worked on generateHTML.js file

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,1 +1,18 @@
-const
+const manager = managerInput => {
+    return `
+    <div id="${managerData.getRole()}-card" class="box card">
+      <div class="box name-role manager-name">
+        <h2>${managerData.getName()}</h2>
+        <h3>Role: ${managerData.getRole()}</h3>
+      </div> 
+      <div class="box employee-info">
+        <ul class="list-group">
+          <li class="list-group-item">ID: ${managerData.getId()}</li>
+          <li class="list-group-item">Email: <a href="mailto:${managerData.getEmail()}">${managerData.getEmail()}</a></li>
+          <li class="list-group-item">Office Number: ${managerData.getOfficeNumber()}</li>
+        </ul>
+      </div>
+    </div>
+  `
+}
+
